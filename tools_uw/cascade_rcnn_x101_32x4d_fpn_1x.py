5ab2369a4d7229f1c57762ff88b681ad36384f07
TWSFar <twsfcz@163.com>
2020-03-04 08:00:34 +0800

    update

--- tools_uw/cascade_rcnn_x101_32x4d_fpn_1x.py
+++ tools_uw/cascade_rcnn_x101_32x4d_fpn_1x.py
@@ -166,7 +166,7 @@ img_norm_cfg = dict(
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(1000, 800), keep_ratio=True),
+    dict(type='Resize', img_scale=(1024, 1024), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -177,7 +177,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1000, 800),
+        img_scale=(1024, 1024),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
@@ -193,12 +193,12 @@ data = dict(
     workers_per_gpu=1,
     train=dict(
         type=dataset_type,
-        ann_file=data_root + 'Annotation_json/instances_train.json',
+        ann_file=data_root + 'Annotations_json/instances_train.json',
         img_prefix=data_root + 'JPEGImages',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'Annotation_json/instances_val.json',
+        ann_file=data_root + 'Annotations_json/instances_val.json',
         img_prefix=data_root + 'JPEGImages',
         pipeline=test_pipeline),
     test=dict(
@@ -208,7 +208,7 @@ data = dict(
         pipeline=test_pipeline))
 evaluation = dict(interval=1, metric='bbox')
 # optimizer
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.0002, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
